b15757bef5c90bc6d0cea869dae9038c66ee3c99
Shahryar123 <k173702@nu.edu.pk>
2023-05-22 10:05:47 +0500

    first commit

---
+++ EmailWebApi/Services/IMailService.cs
@@ -0,0 +1,10 @@
+﻿using EmailWebApi.Models;
+
+namespace EmailWebApi.Services
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(MailRequest mailRequest);
+        Task SendWelcomeEmailAsync(WelcomeRequest request);
+    }
+}
